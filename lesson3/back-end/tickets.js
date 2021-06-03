259742f613207a149f06f09627ad42c2029c2272
NathanDavisBarrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2021-06-03 11:32:47 -0600

    setup backend

---
+++ lesson3/back-end/tickets.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://mongodb.nathandavisbarrett.com:27017/test', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const ticketSchema = new mongoose.Schema({
+  name: String,
+  problem: String,
+});
+
+ticketSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+ticketSchema.set('toJSON', {
+  virtuals: true,
+});
+
+const Ticket = mongoose.model('Ticket', ticketSchema);
